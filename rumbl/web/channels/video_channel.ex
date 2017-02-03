60aa530c92f1bb3c72bac87602004f2704aa6c21
y-fujiwara <fuji.ys5234@gmail.com>
2017-02-04 00:14:03 +0900

    add function

--- rumbl/web/channels/video_channel.ex
+++ rumbl/web/channels/video_channel.ex
@@ -6,20 +6,36 @@ defmodule Rumbl.VideoChannel do
     # send_interval/2関数は最終的にはsend_interval(Time, self(), Message)という形で呼び出される
     # :timer.send_interval(5_000, :ping)
     # socket.assignsにvideo_idを保存
-    {:ok, socket}
+    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+  end
+
+  def handle_in(event, params, socket) do
+    user = Repo.get(Rumbl.User, socket.assigns.user_id)
+    handle_in(event, params, user, socket)
   end
 
   # クライアントから直接送信された時に受け取るコールバック
-  def handle_in("new_annotation", params, socket) do
-    # 接続しているクライアント全てにブロードキャストする
-    # ユーザが任意のメッセージを送れないようにparamsを分解する
-    broadcast! socket, "new_annotation", %{
-      user: %{username: "anon"},
-      body: params["body"], # jsonがそのままMapかなんかになっているらしい
-      at: params["at"]
-    }
+  def handle_in("new_annotation", params, user, socket) do
+    changeset =
+      user
+      |> build_assoc(:annotations, video_id: socket.assigns.video_id)
+      |> Rumbl.Annotation.changeset(params)
 
-    {:reply, :ok, socket}
+    case Repo.insert(changeset) do
+      {:ok, annotation} ->
+        # 接続しているクライアント全てにブロードキャストする
+        # ユーザが任意のメッセージを送れないようにparamsを分解する
+        broadcast! socket, "new_annotation", %{
+          id: annotation.id,
+          user: Rumbl.UserView.render("user.json", %{user: user}),
+          body: annotation.body,
+          at: annotation.at
+        }
+        {:reply, :ok, socket}
+
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 
   # OTPのコールバックhandle_castやhandle_callの仲間
--- rumbl/web/views/user_view.ex
+++ rumbl/web/views/user_view.ex
@@ -7,4 +7,9 @@ defmodule Rumbl.UserView do
     |> String.split(" ")
     |> Enum.at(0)
   end
+
+  # json版のrender関数を定義 元々のテンプレート名を受け付けるものとのパターンマッチ
+  def render("user.json", %{user: user}) do
+    %{id: user.id, username: user.username}
+  end
 end
