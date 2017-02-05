54975fca565bcc3a3db61635a9a6c3524984b010
y-fujiwara <fuji.ys5234@gmail.com>
2017-02-06 00:42:33 +0900

    add GenServer Counter

--- rumbl/lib/rumbl/counter.ex
+++ rumbl/lib/rumbl/counter.ex
@@ -1,37 +1,45 @@
 defmodule Rumbl.Counter do
   use GenServer
-  
-  # listenにメッセージを送信する
-  def inc(pid), do: send(pid, :inc)
-  
-  def dec(pid), do: send(pid, :dec)
 
-  # listenで保持されている状態を取得する
-  def val(pid, timeout \\ 5000) do
-    # プロセスにリファレンスという形でマークを付ける
-    # リクエストに対してレスポンスを紐付ける
-    ref = make_ref()
-    send(pid, {:val, self(), ref})
-    receive do
-      {^ref, val} -> val
-    after
-      timeout -> exit(:timeout)
-    end
+  # listenにメッセージを送信する
+  def inc(pid), do: GenServer.cast(pid, :inc)
+  
+  def dec(pid), do: GenServer.cast(pid, :dec)
+
+  def val(pid) do
+    # 値が返ってくるのを待つ必要があるため同期呼び出し
+    GenServer.call(pid, :val)
   end
 
   # エントリポイント
   def start_link(initial_val) do
-    {:ok, spawn_link(fn -> listen(initial_val) end)}
+    GenServer.start_link(__MODULE__, initial_val)
   end
 
-  # 無限ループでval状態を保持する
-  defp listen(val) do
-    receive do
-      :inc -> listen(val + 1)
-      :dec -> listen(val - 1)
-      {:val, sender, ref} ->
-        send sender, {ref, val}
-        listen(val)
-    end
+  def init(initial_val) do
+    # :tickメッセージを1000ミリ秒後に自分自身に送信
+    Process.send_after(self(), :tick, 1000)
+    {:ok, initial_val}
+  end
+
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
+
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
+  end
+  
+  # valが0以下になったらわざとクラッシュさせる
+  def handle_info(:tick, val) when val <= 0, do: raise "boom!"
+  # send_afterで自分自身に送られたものを受け取る
+  def handle_info(:tick, val) do
+    IO.puts "tick #{val}"
+    Process.send_after(self(), :tick, 1000)
+    {:noreply, val - 1}
   end
 end
