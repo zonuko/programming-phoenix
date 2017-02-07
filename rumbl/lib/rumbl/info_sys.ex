f93c2e16ee7fc18fc0641439b12dc630850175e3
y-fujiwara <fuji.ys5234@gmail.com>
2017-02-08 00:14:12 +0900

    add monitor

--- rumbl/lib/rumbl/info_sys.ex
+++ rumbl/lib/rumbl/info_sys.ex
@@ -19,6 +19,9 @@ defmodule Rumbl.InfoSys do
     # 各バックエンドサービスに関してプロセスを開始する
     backends
     |> Enum.map(&spawn_query(&1, query, limit))
+    |> await_results(opts)
+    |> Enum.sort(&(&1.score >= &2.score))
+    |> Enum.take(limit)
   end
 
   defp spawn_query(backend, query, limit) do
@@ -28,6 +31,34 @@ defmodule Rumbl.InfoSys do
     # 起動済みのSupervisorに自分自身のプロセスを子として監視してもらう
     # これを呼び出すと自動でstart_linkが呼び出されてプロセス開始する
     {:ok, pid} = Supervisor.start_child(Rumbl.InfoSys.Supervisor, opts)
-    {pid, query_ref}
+
+    # プロセスの死活監視
+    monitor_ref = Process.monitor(pid)
+
+    {pid, monitor_ref, query_ref}
+  end
+
+  defp await_results(children, _opts) do
+    await_results(children, [], :infinity)
+  end
+
+  defp await_results([head|tail], acc, timeout) do
+    {pid, monitor_ref, query_ref} = head
+
+    # wolframなどでsendされた結果を待ち受けてパターンマッチする
+    receive do
+      {:results, ^query_ref, results} ->
+        Process.demonitor(monitor_ref, [:flush])
+        # 再帰でmapの結果を処理する
+        await_results(tail, results ++ acc, timeout)
+      {:DOWN, ^monitor_ref, :process, ^pid, _reason} ->
+        # モニタリングの結果失敗していた時
+        await_results(tail, acc, timeout)
+    end
+  end
+
+  defp await_results([], acc, _) do
+    # 最終的には結果を合体したものを返す
+    acc
   end
 end
