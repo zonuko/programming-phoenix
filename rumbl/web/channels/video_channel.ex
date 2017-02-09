812e844a422cb717d34a5f374fa03d665a9804a8
y-fujiwara <y-fujiwara@ttc-net.co.jp>
2017-02-09 12:54:59 +0900

    Rumblに組み込んだ

--- rumbl/web/channels/video_channel.ex
+++ rumbl/web/channels/video_channel.ex
@@ -38,28 +38,43 @@ defmodule Rumbl.VideoChannel do
       |> Rumbl.Annotation.changeset(params)
 
     case Repo.insert(changeset) do
-      {:ok, annotation} ->
-        # 接続しているクライアント全てにブロードキャストする
-        # ユーザが任意のメッセージを送れないようにparamsを分解する
-        broadcast! socket, "new_annotation", %{
-          id: annotation.id,
-          user: Rumbl.UserView.render("user.json", %{user: user}),
-          body: annotation.body,
-          at: annotation.at
-        }
+      {:ok, ann} ->
+        # コメントを取り敢えず保存
+        broadcast_annotation(socket, ann)
+        # コメントに対するInfoSysの結果を取得する(非同期)
+        # 取得結果はwolframユーザのannotationとして保存される
+        Task.start_link(fn -> compute_additional_info(ann, socket) end)
         {:reply, :ok, socket}
-
       {:error, changeset} ->
         {:reply, {:error, %{errors: changeset}}, socket}
     end
   end
 
-  # OTPのコールバックhandle_castやhandle_callの仲間
-  # castやcallで処理される以外のメッセージを処理するらしい
-  # def handle_info(:ping, socket) do
-  #   count = socket.assigns[:count] || 1
-  #   push socket, "ping", %{count: count}
+  defp compute_additional_info(ann, socket) do
+    # computeには結果をスコア順で先頭一つだけ取るように指示
+    # googleとかの結果もほしいならlimit2とかにすれば良いはず 
+    # 結果は要らないのでリスト内包表記の結果は呼び出し元でも受け取っていない
+    for result <- Rumbl.InfoSys.compute(ann.body, limit: 1, timeout: 10_000) do
+      attrs = %{url: result.url, body: result.text, at: ann.at}
 
-  #   {:noreply, assign(socket, :count, count + 1)}
-  # end
+      info_changeset = 
+        Repo.get_by!(Rumbl.User, username: result.backend) # ユーザを取得
+        |> build_assoc(:annotations, video_id: ann.video_id) # ユーザに紐づくannotationを作成
+        |> Rumbl.Annotation.changeset(attrs) # annotationのchangesetを作成
+
+      case Repo.insert(info_changeset) do
+        # インサート出来たらInfoSysの結果を共通関数でブロードキャストする
+        {:ok, info_ann} -> broadcast_annotation(socket, info_ann)
+        {:error, _changeset} -> :ignore
+      end
+    end
+  end
+  
+  defp broadcast_annotation(socket, annotation) do
+    annotation = Repo.preload(annotation, :user)
+    rendered_ann = Phoenix.View.render(AnnotationView, "annotation.json", %{
+      annotation: annotation
+    })
+    broadcast! socket, "new_annotation", rendered_ann
+  end
 end
